be29e55dbff8fa05de74d0e5d6188b32eaca287a
Angelo Lakra <angelo.lakra@gmail.com>
2014-07-28 12:28:28 -0600

    Adding some documentation on the WeatherForecasts module

--- lib/weather_forecasts.rb
+++ lib/weather_forecasts.rb
@@ -1,17 +1,36 @@
+# The weather forecasts library retrieves weather forecast data from
+# NOAA.  The dataset that is specifically being retrieved is from the
+# NDFD ( National Digital Forecast Database )/
+#
+# Author::    Angelo Lakra  (mailto:angelo.lakra@gmail.com)
+# License::   MIT License
+
 module WeatherForecasts
   module_function
 
+  # Returns the currently set environment (`:test, :development or
+  # :production`)
+  #
+  # This can be set from the shell environment using the `ENV`
+  # variable.
+
   def env
-    @@env ||= (ENV['ENV'] || :production)
+    @@env ||= (ENV['ENV'].to_sym || :production)
   end
+
+  # Returns a new instance of WeatherForecasts::Client
 
   def client(options = {})
     Client.new(options)
   end
 
+  # Returns the root path from the install path of the project
+
   def root_path
     File.expand_path(File.join(File.dirname(__FILE__), "../"))
   end
+
+  # Returns the vendor path from the install path of the project
 
   def vendor_path
     File.join(root_path, "vendor")
